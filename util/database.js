2fe313951a7477f1e697d04ab9daa1685ec2260f
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-05-07 22:40:18 +0200

    Remove mongodb protocol during connect.

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ import {MongoClient} from 'mongodb';
 import Q from 'q';
 
 export default function connect(collectionId) {
-    return Q.nfcall(MongoClient.connect, 'mongodb://' + process.env.MONGOLAB_TECH_AUTH_URI)
+    return Q.nfcall(MongoClient.connect, process.env.MONGOLAB_TECH_AUTH_URI)
         .then((db) => {
             return Q.nfcall(db.collection.bind(db), collectionId);
         })
