85cddfba1ecb7474cedeab45a8f45685f2ff19c4
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-05-07 22:51:20 +0200

    Try a global try..catch in loginController.

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,30 +1,35 @@
 import database from '../util/database';
 
 export default function loginController(request, reply) {
-    var t = request.auth.credentials;
+    try {
+        var t = request.auth.credentials;
 
-    var profile = {
-        token: t.token,
-        secret: t.secret,
-        id: t.profile.id,
-        name: t.profile.name,
-        fullName: t.profile.displayName,
-    };
+        var profile = {
+            token: t.token,
+            secret: t.secret,
+            id: t.profile.id,
+            name: t.profile.name,
+            fullName: t.profile.displayName,
+        };
 
-    database('profileCollection')
-        .then((collection) => {
-            collection.insert(profile);
-        })
-        .fail((err) => {
-            console.log(err);
-        });
+        database('profileCollection')
+            .then((collection) => {
+                collection.insert(profile);
+            })
+            .fail((err) => {
+                console.log(err);
+            });
 
-    /*request.auth.session.clear();
-    request.auth.session.set(profile);*/
+        /*request.auth.session.clear();
+        request.auth.session.set(profile);*/
 
-    if (!process.env.LOGIN_REDIRECT_URL) {
-        return reply.redirect('/');
+        if (!process.env.LOGIN_REDIRECT_URL) {
+            return reply.redirect('/');
+        }
+
+        return reply.redirect(process.env.LOGIN_REDIRECT_URL + '?token=' + t.token);
+    } catch (err) {
+        console.log(err);
+        return reply().status(404);
     }
-
-    return reply.redirect(process.env.LOGIN_REDIRECT_URL + '?token=' + t.token);
 }
