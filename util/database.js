c14036d362bd854ec104e7288fd0b99ba2258f6e
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-05-07 21:52:06 +0200

    Log database errors.

--- util/database.js
+++ util/database.js
@@ -5,5 +5,8 @@ export default function connect(collectionId) {
     return Q.nfcall(MongoClient.connect, 'mongodb://' + process.env.MONGOLAB_TECH_AUTH_URI)
         .then((db) => {
             return Q.nfcall(db.collection.bind(db), collectionId);
+        })
+        .fail((err) => {
+            console.log(err);
         });
 };
