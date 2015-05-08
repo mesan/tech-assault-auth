f7a25b1e4ece5828680f0ec2c9c03f2f69d83e5b
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-05-08 20:57:32 +0200

    Remove sessions in registry that are older than a certain given time (TTL).

--- controllers/getSessionController.js
+++ controllers/getSessionController.js
@@ -1,16 +1,28 @@
 import database from '../util/database';
+import moment from 'moment';
+import Q from 'q';
 
 export default function getSessionController(request, reply) {
-
     database('profileCollection')
         .then((collection) => {
-            collection.find({
-                    token: request.params.token
-                }, { name: 1, fullName: 1, createdAt: 1 }).toArray((err, docs) => {
+            
+            let oneMonthAgo = moment()
+                .subtract(process.env.TECH_AUTH_TOKEN_TTL, 'seconds')
+                .toDate();
 
+            collection.remove({ createdAt: { $lte: oneMonthAgo }}, (err, docs) => {
+                if (err) {
+                    console.log(err);
+                    return reply(err).code(500);
+                }
+
+                collection.find(
+                    { token: request.params.token },
+                    { name: 1, fullName: 1, createdAt: 1 })
+                .toArray((err, docs) => {
                     if (err) {
                         console.log(err);
-                        return reply().code(204);
+                        return reply(err).code(500);
                     }
 
                     if (!docs.length) {
@@ -19,6 +31,7 @@ export default function getSessionController(request, reply) {
 
                     reply(docs[0]);
                 });
+            });
         })
         .fail((err) => {
             console.log(err);
