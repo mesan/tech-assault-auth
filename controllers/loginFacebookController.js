413d72e84634ea0a025b2a785c45ec9cf8e4c9fd
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-05-11 18:09:34 +0200

    Add auth provider prefix to provider ID instead of token.

--- controllers/loginFacebookController.js
+++ controllers/loginFacebookController.js
@@ -1,7 +1,7 @@
 import database from '../util/database';
 
 let {
-    TECH_AUTH_FACEBOOK_LOGIN_TOKEN_PREFIX,
+    TECH_AUTH_ID_PREFIX_FACEBOOK,
     TECH_AUTH_LOGIN_REDIRECT_URL
 } = process.env;
 
@@ -11,9 +11,9 @@ export default function loginFacebookController(request, reply) {
     let avatar = `http://graph.facebook.com/${cred.profile.id}/picture`;
 
     let profile = {
-        token: TECH_AUTH_FACEBOOK_LOGIN_TOKEN_PREFIX + cred.token,
+        token: cred.token,
         secret: cred.secret,
-        id: cred.profile.id,
+        id: TECH_AUTH_ID_PREFIX_FACEBOOK + cred.profile.id,
         name: cred.profile.name,
         fullName: cred.profile.displayName,
         createdAt: new Date(),
